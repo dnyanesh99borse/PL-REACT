04fba2724b2846dcdd4bd0baada0cef5e490b10b
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2024-11-02 06:23:03 +0530

    footer structure ready

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import '../footer/footer.css';
+import image2 from '../assets/footer image 2.png'
+import backgroundImage from '../assets/wavefinal.png';
+import arrow from '../assets/arrow.png';
+import logo from '../assets/brand.png';
 
-import backgroundImage from '../assets/footerw3.svg';
 
-
-const Footer = () =>{
-    return(
+const Footer = () => {
+    return (
         <div className="footer">
+            <img src={backgroundImage} alt='image' />
             <div className="footercontainer">
                 <div className="left">
-
+                    <img src={image2} alt='image' />
                 </div>
                 <div className="center">
+                    <div className='box1'>
+                        <img src={arrow} alt='arrow' />
+                    </div>
+                    <div className='box2'>
 
+                    </div>
                 </div>
                 <div className="right">
+                <div className='box1'>
+                        <img src={logo} alt='arrow' />
+                    </div>
+                    <div className='box2'>
 
+                    </div>
                 </div>
             </div>
         </div>
