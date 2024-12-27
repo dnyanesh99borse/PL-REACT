e5df1de688ce71f8538a8ca524521aa88b3d991a
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2024-12-27 21:50:30 +0530

    ab padhenge bhi aur badhenge bhi

--- frontend/src/components/header/sidenavbar.js
+++ frontend/src/components/header/sidenavbar.js
@@ -1,30 +1,32 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate from React Router
 import './sidenavbar.css';
+
 const SideNavbar = () => {
+    const navigate = useNavigate(); // Initialize the navigate function
+
     const navigateToPage = (page) => {
-        // Implement navigation logic here
-        console.log(`Navigating to ${page}`);
-        // For example, you could use window.location.href or a React Router navigation here
+        navigate(page); // Use React Router's navigate function
     };
 
     return (
         <div className='sidenavbar'>
             <div className="container">
-                <div className="option" id="profile" onClick={() => navigateToPage('profile.ejs')}>
+                <div className="option" id="profile" onClick={() => navigateToPage('/profile')}>
                     <span className="icon" id="profile-text">
                         <i className='bx bx-user-circle'></i>
                     </span>
                     <p className="text" id="profile-text">profile</p>
                 </div>
 
-                <div className="option" id="order" onClick={() => navigateToPage('order.ejs')}>
+                <div className="option" id="order" onClick={() => navigateToPage('/order')}>
                     <span className="icon">
                         <i className="bx bx-basket"></i>
                     </span>
                     <p className="text">order</p>
                 </div>
 
-                <div className="option" id="mybag" onClick={() => navigateToPage('../')}>
+                <div className="option" id="mybag" onClick={() => navigateToPage('/mybag')}>
                     <span className="icon">
                         <i className="bx bxs-backpack"></i>
                     </span>
